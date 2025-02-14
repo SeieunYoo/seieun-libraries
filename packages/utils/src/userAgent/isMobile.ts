0b7eaab453bc212952699f8d6567f731f4da4030
SeieunYoo <isho48239@gmail.com>
2025-02-14 23:01:48 +0900

    feat: utils 에 주석 추가

--- packages/utils/src/userAgent/isMobile.ts
+++ packages/utils/src/userAgent/isMobile.ts
@@ -1,5 +1,9 @@
-import { isServer } from "./isServer.js";
+import { isServer } from "./isServer";
 
+/**
+ * @description 사용자의 유저 에이전트가 모바일인지 확인합니다.
+ * @returns {boolean}
+ */
 export const isMobileUser = () => {
   if (isServer()) {
     return false;
--- packages/utils/src/userAgent/isServer.ts
+++ packages/utils/src/userAgent/isServer.ts
@@ -1,3 +1,7 @@
+/**
+ * @description 서버 환경인지 확인합니다.
+ * @returns {boolean}
+ */
 export const isServer = () => {
   return typeof window === "undefined";
 };
